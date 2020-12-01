83ddf5ad1dbf8ba01d74f846d0087a6659fa1046
ik1xpv <ik1xpv@gmail.com>
2020-12-01 11:45:47 +0100

    circular shift tune
    circular shift tune fs/2 fixed

--- ExtIO_sddc/r2iq.cpp
+++ ExtIO_sddc/r2iq.cpp
@@ -354,7 +354,7 @@ void * r2iqControlClass::r2iqThreadf(r2iqThreadArg *th) {
 		{
 			// FFT first stage time to frequency, real to complex
 			fftwf_execute_dft_r2c(th->plan_t2f_r2c, th->ADCinTime[k], th->ADCinFreq);
-
+			/*
 			if (LWzero)
 			{
 				th->inFreqTmp[0][0] = th->inFreqTmp[0][1] = 0;  // bin[0] = 0;
@@ -372,42 +372,35 @@ void * r2iqControlClass::r2iqThreadf(r2iqThreadArg *th) {
 				}
 			}
 			else
+			*/
 			{
 				for (int m = 0; m < mfft / 2; m++) // circular shift tune fs/2 half array
 				{
-					if ((_mtunebin + m) < mfft)
-					{
-						th->inFreqTmp[m][0] = (th->ADCinFreq[_mtunebin + m][0] * filter[m][0] +
-							th->ADCinFreq[_mtunebin + m][1] * filter[m][1]);
-						th->inFreqTmp[m][1] = (th->ADCinFreq[_mtunebin + m][1] * filter[m][0] -
-							th->ADCinFreq[_mtunebin + m][0] * filter[m][1]);
-					}
-					else
-					{
-						th->inFreqTmp[m][0] = 0;
-						th->inFreqTmp[m][1] = 0;
-					}
+					int mm = _mtunebin + m;
+					if ((_mtunebin + m) < mfft /2)
+						mm -= - mfft /2;
+
+					th->inFreqTmp[m][0] = (th->ADCinFreq[_mtunebin + m][0] * filter[m][0] +
+						th->ADCinFreq[_mtunebin + m][1] * filter[m][1]);
+
+					th->inFreqTmp[m][1] = (th->ADCinFreq[_mtunebin + m][1] * filter[m][0] -
+						th->ADCinFreq[_mtunebin + m][0] * filter[m][1]);
+
 
 				}
 				for (int m = 0; m < mfft / 2; m++) // circular shift tune fs/2 half array
-				{
-
-					int mm = _mtunebin - mfft / 2 + m;
+				{				
 					int fm = halfFft - mfft / 2 + m;
-					if (mm >= 0) // corrects off limits
-					{
-						th->inFreqTmp[mfft / 2 + m][0] = (th->ADCinFreq[mm][0] * filter[fm][0] +
-							th->ADCinFreq[mm][1] * filter[fm][1]);
+					int mm = _mtunebin - mfft / 2 + m;
+					if (mm < 0)
+						mm += mfft;
+					
+					th->inFreqTmp[mfft / 2 + m][0] = (th->ADCinFreq[mm][0] * filter[fm][0] +
+						th->ADCinFreq[mm][1] * filter[fm][1]);
 
-						th->inFreqTmp[mfft / 2 + m][1] = (th->ADCinFreq[mm][1] * filter[fm][0] -
-							th->ADCinFreq[mm][0] * filter[fm][1]);
-					}
-					else
-					{
-						th->inFreqTmp[mfft / 2 + m][0] = 0;
-						th->inFreqTmp[mfft / 2 + m][1] = 0;
-					}
-
+					th->inFreqTmp[mfft / 2 + m][1] = (th->ADCinFreq[mm][1] * filter[fm][0] -
+						th->ADCinFreq[mm][0] * filter[fm][1]);
+					
 				}
 			}
 
