0c92a596f3288c718976c23cacc801c2a4bba1bd
Howard Su <howard0su@gmail.com>
2020-12-11 07:40:35 +0800

    fix header
    replace t -> this

--- libsddc/libsddc.h
+++ libsddc/libsddc.h
@@ -4,18 +4,18 @@
  *
  * Copyright (C) 2020 by Franco Venturi
  *
- * t program is free software: you can redistribute it and/or modify
+ * this program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * t program is distributed in the hope that it will be useful,
+ * this program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with t program.  If not, see <https://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
