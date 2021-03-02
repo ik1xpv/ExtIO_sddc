11794faa89270630eb6a5bf26e3013566f0adc73
Oscar Steila <ik1xpv@users.noreply.github.com>
2021-03-02 15:56:43 +0100

    Reference PPM calibration by HDSDR (#174)
    * Fix GetPPMvalue
    
    * add updateppm
    
    * Fix HDSDR version test

--- ExtIO_sddc/resource.h
+++ ExtIO_sddc/resource.h
@@ -8,6 +8,7 @@
 #define IDB_BITMAP1                     105
 #define IDB_BITMAP2                     106
 #define IDD_DLG_HDSDR                   107
+#define IDD_DLG_HDSDR281                108
 #define DIALOGH                         80
 #define IDC_LW                          1001
 #define IDC_HF                          1002
