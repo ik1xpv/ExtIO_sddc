ab0148a4cebad6075be1d73e40c3b490ff50d914
Howard Su <howard0su@gmail.com>
2021-04-27 23:11:27 +0800

    reformat the code a bit

--- Core/fft_mt_r2iq_impl.hpp
+++ Core/fft_mt_r2iq_impl.hpp
@@ -1,25 +1,25 @@
 
 {
-	fftwf_complex *inFreqTmp;         // tmp decimation output buffers (after tune shift)
+	fftwf_complex *inFreqTmp; // tmp decimation output buffers (after tune shift)
 
 	const int decimate = this->mdecimation;
-	const int mfft = this->mfftdim[decimate];	// = halfFft / 2^mdecimation
-	const fftwf_complex* filter = filterHw[decimate];
+	const int mfft = this->mfftdim[decimate]; // = halfFft / 2^mdecimation
+	const fftwf_complex *filter = filterHw[decimate];
 	const bool lsb = this->getSideband();
 	const auto filter2 = &filter[halfFft - mfft / 2];
 
-
-	inFreqTmp = (fftwf_complex*)fftwf_malloc(sizeof(fftwf_complex)*(halfFft));    // 1024
+	inFreqTmp = (fftwf_complex *)fftwf_malloc(sizeof(fftwf_complex) * (halfFft)); // 1024
 	fftwf_plan plan_f2t_c2c = fftwf_plan_dft_1d(mfftdim[decimate], inFreqTmp, inFreqTmp, FFTW_BACKWARD, FFTW_MEASURE);
 
 	int decimate_count = 0;
-	fftwf_complex* pout = nullptr;
+	fftwf_complex *pout = nullptr;
 
-	while (r2iqOn) {
-		const int16_t *dataADC;  // pointer to input data
-		const int16_t *endloop;    // pointer to end data to be copied to beginning
+	while (r2iqOn)
+	{
+		const int16_t *dataADC; // pointer to input data
+		const int16_t *endloop; // pointer to end data to be copied to beginning
 
-		const int _mtunebin = this->mtunebin;  // Update LO tune is possible during run
+		const int _mtunebin = this->mtunebin; // Update LO tune is possible during run
 
 		dataADC = inputbuffer->getReadPtr();
 
@@ -39,7 +39,7 @@
 #if PRINT_INPUT_RANGE
 		std::pair<int16_t, int16_t> blockMinMax = std::make_pair<int16_t, int16_t>(0, 0);
 #endif
-		if (!this->getRand())        // plain samples no ADC rand set
+		if (!this->getRand()) // plain samples no ADC rand set
 		{
 			convert_float<false>(endloop, inloop, halfFft);
 #if PRINT_INPUT_RANGE
@@ -59,13 +59,13 @@
 		this->MinValue = std::min(blockMinMax.first, this->MinValue);
 		this->MaxValue = std::max(blockMinMax.second, this->MaxValue);
 		++this->MinMaxBlockCount;
-		if (this->MinMaxBlockCount * processor_count / 3 >= DEFAULT_TRANSFERS_PER_SEC )
+		if (this->MinMaxBlockCount * processor_count / 3 >= DEFAULT_TRANSFERS_PER_SEC)
 		{
 			float minBits = (this->MinValue < 0) ? (log10f((float)(-this->MinValue)) / log10f(2.0f)) : -1.0f;
 			float maxBits = (this->MaxValue > 0) ? (log10f((float)(this->MaxValue)) / log10f(2.0f)) : -1.0f;
 			printf("r2iq: min = %d (%.1f bits) %.2f%%, max = %d (%.1f bits) %.2f%%\n",
-				(int)this->MinValue, minBits, this->MinValue *-100.0f / 32768.0f,
-				(int)this->MaxValue, maxBits, this->MaxValue * 100.0f / 32768.0f);
+				   (int)this->MinValue, minBits, this->MinValue * -100.0f / 32768.0f,
+				   (int)this->MaxValue, maxBits, this->MaxValue * 100.0f / 32768.0f);
 			this->MinValue = 0;
 			this->MaxValue = 0;
 			this->MinMaxBlockCount = 0;
@@ -76,12 +76,12 @@
 		// decimate in frequency plus tuning
 
 		if (decimate_count == 0)
-			pout = (fftwf_complex*)outputbuffer->getWritePtr();
+			pout = (fftwf_complex *)outputbuffer->getWritePtr();
 
 		decimate_count = (decimate_count + 1) & ((1 << decimate) - 1);
 
 		// Calculate the parameters for the first half
-		const auto count = std::min(mfft/2, halfFft - _mtunebin);
+		const auto count = std::min(mfft / 2, halfFft - _mtunebin);
 		const auto source = &this->ADCinFreq[_mtunebin];
 
 		// Calculate the parameters for the second half
@@ -107,7 +107,7 @@
 						memset(inFreqTmp[count], 0, sizeof(float) * 2 * (mfft / 2 - count));
 
 					// circular shift tune fs/2 second half array
-					shift_freq(dest, source2, filter2, start, mfft/2);
+					shift_freq(dest, source2, filter2, start, mfft / 2);
 					if (start != 0)
 						memset(inFreqTmp[mfft / 2], 0, sizeof(float) * 2 * start);
 				}
@@ -115,8 +115,8 @@
 
 				// 'shorter' inverse FFT transform (decimation); frequency (back) to COMPLEX time domain
 				// transform size: mfft = mfftdim[k] = halfFft / 2^k with k = mdecimation
-				fftwf_execute_dft(plan_f2t_c2c, inFreqTmp, inFreqTmp);     //  c2c decimation
-				// result now in inFreqTmp[]
+				fftwf_execute_dft(plan_f2t_c2c, inFreqTmp, inFreqTmp); //  c2c decimation
+																	   // result now in inFreqTmp[]
 			}
 
 			// postprocessing
@@ -138,7 +138,7 @@
 				// mirror just by negating the imaginary Q of complex I/Q
 				if (k == 0)
 				{
-					copy<true>(pout, &inFreqTmp[mfft / 4], mfft/2);
+					copy<true>(pout, &inFreqTmp[mfft / 4], mfft / 2);
 				}
 				else
 				{
@@ -149,7 +149,7 @@
 			{
 				if (k == 0)
 				{
-					copy<false>(pout, &inFreqTmp[mfft / 4], mfft/2);
+					copy<false>(pout, &inFreqTmp[mfft / 4], mfft / 2);
 				}
 				else
 				{
@@ -159,7 +159,8 @@
 			// result now in this->obuffers[]
 		}
 
-		if (decimate_count == 0) {
+		if (decimate_count == 0)
+		{
 			outputbuffer->WriteDone();
 			pout = nullptr;
 		}
@@ -168,7 +169,7 @@
 			pout += mfft / 2 + (3 * mfft / 4) * (fftPerBuf - 1);
 		}
 	} // while(run)
-//    DbgPrintf((char *) "r2iqThreadf idx %d pthread_exit %u\n",(int)this->t, pthread_self());
+	  //    DbgPrintf((char *) "r2iqThreadf idx %d pthread_exit %u\n",(int)this->t, pthread_self());
 
 	fftwf_free(inFreqTmp);
 	fftwf_destroy_plan(plan_f2t_c2c);
