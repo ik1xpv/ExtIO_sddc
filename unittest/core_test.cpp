641331c47f41b723543382e01fcf535e0b74e7c7
Phil Ashby <phil.github@ashbysoft.com>
2021-09-29 01:28:30 +0000

    Arm neon port (#203)
    * Compiles and passes unit tests on ARMv7 (OrangePi)
    
    * Dynamic detection of NEON from hardware
    
    * Rate limit hardware emulator, print counters
    
    * X86_64 testing, portable printf format
    
    * Return to 1 second run time for R2IQ tests
    
    * Remove wisdom files, align printf

--- unittest/core_test.cpp
+++ unittest/core_test.cpp
@@ -3,6 +3,8 @@
 #include "CppUnitTestFramework.hpp"
 #include <thread>
 #include <chrono>
+#include <vector>
+#include <inttypes.h>  // For portable 64-bit type printf codes
 
 #include "RadioHandler.h"
 
@@ -15,27 +17,27 @@ class fx3handler : public fx3class
         return true;
     }
 
-	bool Control(FX3Command command, uint8_t data = 0)
+    bool Control(FX3Command command, uint8_t data = 0)
     {
         return true;
     }
 
-	bool Control(FX3Command command, uint32_t data)
+    bool Control(FX3Command command, uint32_t data)
     {
         return true;
     }
 
-	bool Control(FX3Command command, uint64_t data)
+    bool Control(FX3Command command, uint64_t data)
     {
         return true;
     }
 
-	bool SetArgument(uint16_t index, uint16_t value)
+    bool SetArgument(uint16_t index, uint16_t value)
     {
         return true;
     }
 
-	bool GetHardwareInfo(uint32_t* data) {
+    bool GetHardwareInfo(uint32_t* data) {
         const uint8_t d[4] = {
             0, FIRMWARE_VER_MAJOR, FIRMWARE_VER_MINOR, 0
         };
@@ -50,6 +52,7 @@ class fx3handler : public fx3class
 
     std::thread emuthread;
     bool run;
+	long nxfers;
     void StartStream(ringbuffer<int16_t>& input, int numofblock)
     {
         run = true;
@@ -59,6 +62,7 @@ class fx3handler : public fx3class
                 auto ptr = input.getWritePtr();
                 memset(ptr, 0x5A, input.getWriteCount());
                 input.WriteDone();
+                ++nxfers;
                 std::this_thread::sleep_for(1ms);
             }
         });
@@ -68,6 +72,8 @@ class fx3handler : public fx3class
         run = false;
         emuthread.join();
     }
+public:
+	long Xfers(bool clear) { long rv=nxfers; if (clear) nxfers=0; return rv; }
 };
 
 static uint32_t count;
@@ -135,12 +141,14 @@ TEST_CASE(CoreFixture, R2IQTest)
         count = 0;
         totalsize = 0;
         radio->Start(decimate); // full bandwidth
-        std::this_thread::sleep_for(0.5s);
+        std::this_thread::sleep_for(1s);
         radio->Stop();
 
         REQUIRE_TRUE(count > 0);
         REQUIRE_TRUE(totalsize > 0);
         REQUIRE_EQUAL(totalsize / count, transferSamples / 2);
+        printf("decimate=%d nxfers=%ld count=%u totalsize=%" PRIu64 "\n",
+            decimate, usb->Xfers(true), count, totalsize);
     }
 
     delete radio;
