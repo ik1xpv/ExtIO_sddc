4bfd03c6d8ce1303dde5ee380fa5d635d743629b
ik1xpv <ik1xpv@gmail.com>
2021-11-03 16:14:18 +0100

    Linux build patch
    TODO

--- Core/arch/linux/FX3handler.h
+++ Core/arch/linux/FX3handler.h
@@ -26,6 +26,7 @@ public:
 	bool ReadDebugTrace(uint8_t* pdata, uint8_t len);
 	void StartStream(ringbuffer<int16_t>& input, int numofblock);
 	void StopStream();
+	bool Enumerate(unsigned char &idx, char *lbuf, uint8_t* fw_data, uint32_t fw_size);
 
 private:
 	bool ReadUsb(uint8_t command, uint16_t value, uint16_t index, uint8_t *data, size_t size);
