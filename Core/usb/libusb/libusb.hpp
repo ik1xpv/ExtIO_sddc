6a36f7fa3e1ac5f9ee1dbe8e127de33d53dbf74d
Ruslan Migirov <trapi78@gmail.com>
2023-11-04 00:15:52 +0200

    Fix libusb include on macOS

--- Core/usb/libusb/libusb.hpp
+++ Core/usb/libusb/libusb.hpp
@@ -1,7 +1,10 @@
 #ifndef __LIBUSB_HPP__
 #define __LIBUSB_HPP__
 
-#include <libusb-1.0/libusb.h>
-
+#if defined(_WIN32) || defined(_WIN64)
+    #include <libusb-1.0/libusb.h>
+#else
+    #include <libusb.h>
+#endif
 
 #endif // __LIBUSB_HPP__
