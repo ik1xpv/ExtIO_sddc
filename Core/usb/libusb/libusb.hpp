50ac4f93bc1717b208986e3fcf7189f62eb6059e
Ruslan Migirov <trapi78@gmail.com>
2023-11-04 21:21:15 +0200

    Fix windows build

--- Core/usb/libusb/libusb.hpp
+++ Core/usb/libusb/libusb.hpp
@@ -1,7 +1,7 @@
 #ifndef __LIBUSB_HPP__
 #define __LIBUSB_HPP__
 
-#if defined(_WIN32) && defined(USE_LIBUSB_DRIVER)
+#if defined(_WIN32) 
     #include <libusb-1.0/libusb.h>
 #elif defined(__APPLE__)
     #include <libusb.h>
