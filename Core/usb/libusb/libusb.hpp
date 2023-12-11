decffb2be02e2ddb768b96e2a96b333bab75729d
Ruslan Migirov <trapi78@gmail.com>
2023-12-12 00:26:29 +0200

    save wip

--- Core/usb/libusb/libusb.hpp
+++ Core/usb/libusb/libusb.hpp
@@ -32,7 +32,7 @@ public:
     bool CreateHandle(unsigned char dev);
     bool Open(uint8_t dev);
     
-     libusb_ss_endpoint_companion_descriptor *ep_comp_desc;
+    libusb_ss_endpoint_companion_descriptor *ep_comp_desc;
     libusb_device *device;
     libusb_device_descriptor descriptor;
     libusb_config_descriptor *config;
@@ -42,8 +42,7 @@ public:
     const libusb_interface_descriptor *altsetting;
 
 private:
-    //libusb_context *ctx;
-
+    
     int Devices;
     void GetDevDescriptor(void);
     void GetDeviceName(void);
@@ -57,73 +56,5 @@ public:
     FX3Device();
     ~FX3Device();
 };
-
-typedef struct _WORD_SPLIT {
-    unsigned char lowByte;
-    unsigned char hiByte;
-} WORD_SPLIT, *PWORD_SPLIT;
-
-typedef struct _BM_REQ_TYPE {
-    unsigned char   Recipient:2;
-    unsigned char   Reserved:3;
-    unsigned char   Type:2;
-    unsigned char   Direction:1;
-} BM_REQ_TYPE, *PBM_REQ_TYPE;
-
-typedef struct _SETUP_PACKET {
-
-    union {
-        BM_REQ_TYPE bmReqType;
-        unsigned char bmRequest;
-    };
-
-    unsigned char bRequest;
-
-    union {
-        WORD_SPLIT wVal;
-        unsigned short wValue;
-    };
-
-    union {
-        WORD_SPLIT wIndx;
-        unsigned short wIndex;
-    };
-
-    union {
-        WORD_SPLIT wLen;
-        unsigned short wLength;
-    };
-
-    unsigned long ulTimeOut;
-
-} SETUP_PACKET, *PSETUP_PACKET;
-
-typedef struct _ISO_ADV_PARAMS {
-
-    unsigned short isoId;
-    unsigned short isoCmd;
-
-    unsigned long ulParam1;
-    unsigned long ulParam2;
-
-} ISO_ADV_PARAMS, *PISO_ADV_PARAMS;
-
-
-typedef struct _SINGLE_TRANSFER {
-    union {
-        SETUP_PACKET SetupPacket;
-        ISO_ADV_PARAMS IsoParams;
-    };
-
-    unsigned char reserved;
-
-    unsigned char ucEndpointAddress;
-    unsigned long NtStatus;
-    unsigned long UsbdStatus;
-    unsigned long IsoPacketOffset;
-    unsigned long IsoPacketLength;
-    unsigned long BufferOffset;
-    unsigned long BufferLength;
-} SINGLE_TRANSFER, *PSINGLE_TRANSFER;
 
 #endif // __LIBUSB_HPP__
